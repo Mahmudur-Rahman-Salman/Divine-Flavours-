5240ed5878e4e6848ae66f25b216c81cae4334b6
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2025-09-10 16:26:21 +0600

    navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Footer from "./Shared/Footer";
+import Navbar from "./Shared/Navbar";
 
 
 function App() {
   return (
     <>
       <div>
+        <Navbar></Navbar>
         <Outlet></Outlet>
        <Footer></Footer>
       </div>
