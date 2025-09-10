52d8297d2dc7a82a9fb269cb52ddb483b11a2ab7
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2025-09-11 05:25:28 +0600

    category section edited

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
 // import required modules
-import { FreeMode, Pagination } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 
 import slide1 from "../assets/slide1.jpg";
 import slide2 from "../assets/slide2.jpg";
@@ -20,37 +18,73 @@ import SectionTItle from "../Shared/SectionTItle";
 
 const Category = () => {
   return (
-    <div>
-        <SectionTItle subHeading={"From 11.00am to 10.00pm"} heading={"Order Online"}></SectionTItle>
+    <div className="px-4 md:px-10 mb-10">
+      <SectionTItle
+        subHeading={"From 11.00am to 10.00pm"}
+        heading={"Order Online"}
+      ></SectionTItle>
       <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        freeMode={true}
-        pagination={{
-          clickable: true,
+        slidesPerView={1}
+        spaceBetween={20}
+        centeredSlides={true}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          640: { slidesPerView: 2 },
+          768: { slidesPerView: 3 },
+          1024: { slidesPerView: 4 },
         }}
-        modules={[FreeMode, Pagination]}
-        className="mySwiper mb-24"
+        modules={[Pagination]}
+        className="mySwiper"
       >
         <SwiperSlide>
-          <img src={slide1} alt="slider image" />
-          <h3 className="text-4xl uppercase text-center -mt-16 text-white">Salads</h3>
+          <img
+            src={slide1}
+            alt="slider image"
+            className="w-full h-64 object-cover rounded-lg"
+          />
+          <h3 className="text-2xl md:text-3xl lg:text-4xl uppercase text-center -mt-12 md:-mt-16 text-white">
+            Salads
+          </h3>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide2} alt="slider image" />
-          <h3 className="text-4xl uppercase text-center -mt-16 text-white">Pizzas</h3>
+          <img
+            src={slide2}
+            alt="slider image"
+            className="w-full h-64 object-cover rounded-lg"
+          />
+          <h3 className="ttext-2xl md:text-3xl lg:text-4xl uppercase text-center -mt-12 md:-mt-16 text-white">
+            Pizzas
+          </h3>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide3} alt="slider image" />
-          <h3 className="text-4xl uppercase text-center -mt-16 text-white">Soups</h3>
+          <img
+            src={slide3}
+            alt="slider image"
+            className="w-full h-64 object-cover rounded-lg"
+          />
+          <h3 className="text-2xl md:text-3xl lg:text-4xl uppercase text-center -mt-12 md:-mt-16 text-white">
+            Soups
+          </h3>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide4} alt="slider image" />
-          <h3 className="text-4xl uppercase text-center -mt-16 text-white">Desserts</h3>
+          <img
+            src={slide4}
+            alt="slider image"
+            className="w-full h-64 object-cover rounded-lg"
+          />
+          <h3 className="text-2xl md:text-3xl lg:text-4xl uppercase text-center -mt-12 md:-mt-16 text-white">
+            Desserts
+          </h3>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide5} alt="slider image" />
-          <h3 className="text-4xl uppercase text-center -mt-16 text-white">Vegetables</h3>
+          <img
+            src={slide5}
+            alt="slider image"
+            className="w-full h-64 object-cover rounded-lg"
+          />
+          <h3 className="text-2xl md:text-3xl lg:text-4xl uppercase text-center -mt-12 md:-mt-16 text-white">
+            Vegetables
+          </h3>
         </SwiperSlide>
       </Swiper>
     </div>
