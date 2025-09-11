fa6a1ab90b3ef165de7d1052da24211ad3002f30
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2025-09-11 07:55:17 +0600

    featured item was edited

--- frontend/src/components/Featured.jsx
+++ frontend/src/components/Featured.jsx
@@ -8,35 +8,33 @@ const Featured = () => {
       style={{ backgroundImage: `url(${fetatureImg})` }}
       className="bg-cover bg-center text-white pt-8 my-20"
     >
-      <div>
-        <SectionTItle subHeading={"check it out"} heading={"Featured Item"} />
-        <div className="md:flex justify-center items-center gap-8 px-6 md:px-20 lg:px-36 py-12 rounded-lg">
-          {/* Image */}
-          <div className="md:w-1/2 w-full">
-            <img
-              src={fetatureImg}
-              alt="feature item"
-              className="w-full h-auto rounded-lg shadow-lg"
-            />
-          </div>
+      <SectionTItle subHeading={"check it out"} heading={"Featured Item"} />
+      <div className="md:flex justify-center items-center gap-8 px-6 md:px-20 lg:px-36 py-12 ">
+        {/* Image */}
+        <div className="md:w-1/2 w-full">
+          <img
+            src={fetatureImg}
+            alt="feature item"
+            className="w-full h-auto rounded-lg shadow-lg"
+          />
+        </div>
 
-          {/* Text */}
-          <div className="md:w-1/2 w-full mt-6 md:mt-0">
-            <p className="text-gray-200">Aug 20, 2029</p>
-            <p className="uppercase text-xl font-semibold mb-3">
-              Where can I get some?
-            </p>
-            <p className="text-gray-100 leading-relaxed">
-              Discover our chef’s handpicked specials, crafted with love and
-              fresh ingredients. From sizzling hot pizzas to refreshing garden
-              salads, each bite is made to satisfy your cravings. Whether you’re
-              in the mood for something light or indulgent, our featured items
-              bring together the perfect balance of flavor and comfort.
-            </p>
-            <button className="btn btn-outline border-0 border-b-4 mt-6">
-              Order Now
-            </button>
-          </div>
+        {/* Text */}
+        <div className="md:w-1/2 w-full mt-6 md:mt-0">
+          <p className="text-gray-200">Aug 20, 2029</p>
+          <p className="uppercase text-xl font-semibold mb-3">
+            Where can I get some?
+          </p>
+          <p className="text-gray-100 leading-relaxed">
+            Discover our chef’s handpicked specials, crafted with love and fresh
+            ingredients. From sizzling hot pizzas to refreshing garden salads,
+            each bite is made to satisfy your cravings. Whether you’re in the
+            mood for something light or indulgent, our featured items bring
+            together the perfect balance of flavor and comfort.
+          </p>
+          <button className="btn btn-outline border-0 border-b-4 mt-6">
+            Order Now
+          </button>
         </div>
       </div>
     </div>
