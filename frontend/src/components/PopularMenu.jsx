92488db08b6e768a6562a04e30b98f9fc9ae659c
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2025-09-11 06:15:27 +0600

    popular menu items section created

---
+++ frontend/src/components/PopularMenu.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import SectionTItle from "../Shared/SectionTItle";
+import MenuItem from "./MenuItem";
+
+const PopularMenu = () => {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    try {
+      fetch("menu.json")
+        .then((res) => res.json())
+        .then((data) => {
+          const popularItems = data.filter(
+            (item) => item.category === "popular"
+          );
+          setMenu(popularItems);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <section>
+      <SectionTItle
+        heading={"From Our Menu"}
+        subHeading={"Popular Items"}
+      ></SectionTItle>
+      <div className="grid md:grid-cols-2 gap-10 ">
+        {menu.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <button className="btn btn-outline border-0 border-b-4 mt-4 mb-10">
+        View Full Menu
+      </button>
+    </section>
+  );
+};
+
+export default PopularMenu;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banners from '../../components/Banners';
 import Category from '../../components/Category';
+import PopularMenu from '../../components/PopularMenu';
 
 const Home = () => {
     return (
         <div>
             <Banners></Banners>
             <Category></Category>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
